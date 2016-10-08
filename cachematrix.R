5e79f174c8aa6a36790284858a2c1d5e408b04da
gargshi <gargshi@amazon.com>
2016-10-08 15:51:13 -0700

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
+## This method returns a list that contains four methods: get, set, setmatrix, getmatrix, given an input x. 
+## Default value of input variable is empty matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+    set <- function(y) {
+            x <<- y
+            m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(matrix) m <<- matrix
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
+## This method returns an invertible matrix given a matrix as input. 
+## It first looks up value in cache and if it doesn't in cache, it calculates the inverse and saves 
+## the value in the cache and retuns the value
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setmatrix(m)
+    m
 }
